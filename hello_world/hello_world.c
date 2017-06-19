d23ff783ed176b632c9303bcc474c57aadab22a3
Hajime Kurita <hajimekurita@1stclass.co.jp>
2017-06-20 02:32:35 +0800

    added hello world for c and c++

---
+++ hello_world/hello_world.c
@@ -0,0 +1,8 @@
+/* 
+ * cc -o hello_world hello_world.c;./hello_world;rm hello_world;
+ */
+#include<stdio.h>
+int main() {
+    printf("Hello World\n");
+}
+	
---
+++ hello_world/hello_world.cpp
@@ -0,0 +1,7 @@
+/*
+ * g++ -o hello_world hello_world.cpp;./hello_world;rm hello_world;   
+ */
+#include <iostream>
+int main() {
+     std::cout << "Hello World\n";
+}
