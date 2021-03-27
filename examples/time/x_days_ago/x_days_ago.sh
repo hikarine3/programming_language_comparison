9d8aaaedae9b88649ad66f8aa23fb2f75a0d734b
Hajime Kurita <hajimekurita@1stclass.co.jp>
2021-03-27 20:55:51 +0900

    date

--- examples/time/x_days_ago/x_days_ago.sh
+++ examples/time/x_days_ago/x_days_ago.sh
@@ -1,2 +1,7 @@
-DATE=`TZ=JST+48 date '+%Y/%m/%d'`;
-echo $DATE;
+#/bin/bash
+date -d "02/01/2000" 2>: 1>:; VALID=$?;
+if [ $VALID == 1 ]; then
+  echo `date -d '2 days ago' '+%Y/%m/%d'`;
+else
+  echo `TZ=JST+48 date '+%Y/%m/%d'`;
+fi
