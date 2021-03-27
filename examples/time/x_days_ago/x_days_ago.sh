372ba302df1e1e1ce617932fd2d0c71aaa66c8e6
Hajime Kurita <hajimekurita@1stclass.co.jp>
2021-03-27 21:09:30 +0900

    just 2 days ago

--- examples/time/x_days_ago/x_days_ago.sh
+++ examples/time/x_days_ago/x_days_ago.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 date -d "02/01/2000" 2>: 1>:; INVALID=$?;
 if [ $INVALID == 1 ]; then
-  echo `TZ=JST+48 date '+%Y/%m/%d'`;
+  echo `TZ=+48 date '+%Y/%m/%d'`;
 else
   echo `date -d '2 days ago' '+%Y/%m/%d'`;
 fi
